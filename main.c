2c53a2763f4ba2068efa4b35b63ead4ff6d9889e
adamhensem-netizen <adamafiqz159@gmail.com>
2026-05-25 11:21:14 +0800

    Create main.c
    initial: add main.c resort system

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+/* --- Constants --- */
+#define MAX_PACKAGES  20
+#define MAX_BOOKINGS  100
+#define NAME_LEN      50
+#define ID_LEN        10
+#define DATE_LEN      15
+#define CONTACT_LEN   15
+
+/* --- Struct: Package (stored using Array) --- */
+typedef struct {
+    char  packageID[ID_LEN];
+    char  packageName[NAME_LEN];
+    char  location[NAME_LEN];
+    int   durationNights;
+    float pricePerNight;
+    int   maxGuests;
+} Package;
+ 
